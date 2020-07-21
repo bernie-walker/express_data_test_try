0b360f74b30c91194877d0716cc93d4a2a30533b
Bernard Nehal D'Cunha <58025656+bernie-walker@users.noreply.github.com>
2020-07-22 00:19:21 +0530

    added sqlite3 version checker

--- test/testUsers.js
+++ test/testUsers.js
@@ -1,10 +1,13 @@
 const { assert } = require('chai');
 const { initializeDataBase, Users } = require('../dataBaseOperations');
+const exec = require('util').promisify(require('child_process').exec);
 
 describe('Users', function () {
   let users;
 
   before(async () => {
+    const res = await exec('which sqlite3');
+    console.log(res);
     const db = await initializeDataBase(':memory:');
     users = new Users(db);
     return;
