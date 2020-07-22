76c6af718f5c771c2fa78ce16146275017ab0213
Bernard Nehal D'Cunha <58025656+bernie-walker@users.noreply.github.com>
2020-07-22 09:18:09 +0530

    removed exec from test

--- test/testUsers.js
+++ test/testUsers.js
@@ -1,13 +1,10 @@
 const { assert } = require('chai');
 const { initializeDataBase, Users } = require('../dataBaseOperations');
-const exec = require('util').promisify(require('child_process').exec);
 
 describe('Users', function () {
   let users;
 
   before(async () => {
-    const res = await exec('which sqlite3');
-    console.log(res);
     const db = await initializeDataBase(':memory:');
     users = new Users(db);
     return;
